61453e2b9047c6304c6ac0dbfee30d19b05c5365
Ruben <ruben.lucas1997@gmail.com>
2021-03-16 19:30:34 +0100

    🎉 Setup Gradle

---
+++ cinema/src/test/java/nl/utwente/group23/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.utwente.group23.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
