b44073fc439fb29a0b8de663fc85d5eea612a29e
Ruben <ruben.lucas1997@gmail.com>
2021-03-21 19:24:46 +0100

    🍱 Move to Zuul gateway

--- gateway/src/main/java/nl/utwente/gateway/GatewayApplication.java
+++ gateway/src/main/java/nl/utwente/gateway/GatewayApplication.java
@@ -3,7 +3,11 @@ package nl.utwente.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
