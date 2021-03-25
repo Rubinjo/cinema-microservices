159471543b39d9811684cbbb04c807c3fd4e3c64
Bastius98 <b.f.m.vantintelen@student.utwente.nl>
2021-03-25 16:22:33 +0100

    food-service image fixed

--- food-service/src/main/resources/data.sql
+++ food-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO food (id, name, description, image, price) VALUES
-        (1, 'Popcorn mini (sweet)', 'Taste the sweetest popcorn!!! Now special for the small eaters!!!', 'Baby_groot_with_popcorn.jpg', 2.95),
-        (2, 'Popcorn medi (sweet)', 'Taste the sweetest popcorn!!! Now tastefully popped for all!!!', 'Teenage_groot_with_popcorn.jpg', 4.45),
-        (3, 'Popcorn maxi (sweet)', 'Taste the sweetest popcorn!!! Now in extremely large bucket!!!', 'Groot_with_popcorn.jpg', 5.95);
+        (1, 'Popcorn mini (sweet)', 'Taste the sweetest popcorn!!! Now special for the small eaters!!!', 'https://raw.githubusercontent.com/Rubinjo/cinema_microservices/main/resources/Baby_groot_with_popcorn.jpg', 2.95),
+        (2, 'Popcorn medi (sweet)', 'Taste the sweetest popcorn!!! Now tastefully popped for all!!!', 'https://raw.githubusercontent.com/Rubinjo/cinema_microservices/main/resources/Teenage_groot_with_popcorn.jpg', 4.45),
+        (3, 'Popcorn maxi (sweet)', 'Taste the sweetest popcorn!!! Now in extremely large bucket!!!', 'https://raw.githubusercontent.com/Rubinjo/cinema_microservices/main/resources/Groot_with_popcorn.jpg', 5.95);
         
