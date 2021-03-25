5bca323e5e7af47ac0cec475db848bf13515c076
Bastius98 <b.f.m.vantintelen@student.utwente.nl>
2021-03-25 16:11:09 +0100

    Food service updated

--- food-service/src/main/resources/data.sql
+++ food-service/src/main/resources/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO food (id, name, description, image, price) VALUES
-        (1, 'Popcorn mini (sweet)', 'Taste the sweetest popcorn!!! Now special for the small eaters!!!', 'https://media.tenor.com/images/89d518819f132dd82fd31633486bf1f6/tenor.png', 2.95);
+        (1, 'Popcorn mini (sweet)', 'Taste the sweetest popcorn!!! Now special for the small eaters!!!', 'Baby_groot_with_popcorn.jpg', 2.95),
+        (2, 'Popcorn medi (sweet)', 'Taste the sweetest popcorn!!! Now tastefully popped for all!!!', 'Teenage_groot_with_popcorn.jpg', 4.45),
+        (3, 'Popcorn maxi (sweet)', 'Taste the sweetest popcorn!!! Now in extremely large bucket!!!', 'Groot_with_popcorn.jpg', 5.95);
         
